ba7240b9d56147b61551136ebeee81fdbf9c5515
Hans <hans.lienhop@gmail.com>
2019-11-06 16:48:02 +0100

    Vonny working

---
+++ VONNY/vonny.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+int countPossibilities = 0;
+int inputField[7][8];
+bool cellUsed[7][8];
+bool stoneUsed[7][7];
+
+void solve(int i, int j, int counter)
+{
+	if (counter == 28)
+	{
+		countPossibilities++;
+		return;
+	}
+
+	cellUsed[i][j] = true;
+	int firstSquare, secondSquare;
+	///check HORIZONTAL
+	if (j < 7)
+	{
+		if (inputField[i][j] > inputField[i][j + 1])
+		{
+			firstSquare = inputField[i][j + 1];
+			secondSquare = inputField[i][j];
+		}
+		else
+		{
+			firstSquare = inputField[i][j];
+			secondSquare = inputField[i][j + 1];
+		}
+
+		if (stoneUsed[firstSquare][secondSquare] == false && cellUsed[i][j + 1]==false)
+		{
+			stoneUsed[firstSquare][secondSquare] = true;
+			cellUsed[i][j + 1] = true;
+			int next_i = 0, next_j = 0;
+			bool found = false;
+			for (int _i = i; _i < 7; _i++)
+			{
+				for (int _j = 0; _j < 8; _j++)
+				{
+					if (cellUsed[_i][_j] == false)
+					{
+						next_i = _i, next_j = _j;
+						found = true;;
+						break;
+					}
+				}
+				if (found == true)
+					break;
+			}
+
+			solve(next_i, next_j, counter + 1);
+			cellUsed[i][j + 1] = false;
+			stoneUsed[firstSquare][secondSquare] = false;
+		}
+	}
+	///check VERTICAL
+	if (i < 6)
+	{
+		if (inputField[i][j] > inputField[i + 1][j])
+		{
+			firstSquare = inputField[i + 1][j];
+			secondSquare = inputField[i][j];
+		}
+		else
+		{
+			firstSquare = inputField[i][j];
+			secondSquare = inputField[i + 1][j];
+		}
+		bool found = false;
+		if (stoneUsed[firstSquare][secondSquare] == false && cellUsed[i + 1][j] == false)
+		{
+			stoneUsed[firstSquare][secondSquare] = true;
+			cellUsed[i + 1][j] = true;
+			int next_i = 0, next_j = 0;
+			for (int _i = i; _i < 7; _i++)
+			{
+				for (int _j = 0; _j < 8; _j++)
+				{
+					if (cellUsed[_i][_j] == false)
+					{
+						next_i = _i, next_j = _j;
+						found = true;
+						break;
+					}
+				}
+				if (found == true)
+					break;
+			}
+			//if (found == true)
+			solve(next_i, next_j, counter + 1);
+			cellUsed[i + 1][j] = false;
+			stoneUsed[firstSquare][secondSquare] = false;
+		}
+	}
+	cellUsed[i][j] = false;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				cin >> inputField[i][j];
+			}
+		}
+
+		countPossibilities = 0;
+		solve(0, 0, 0);
+		cout << countPossibilities << endl;
+	}
+	return 0;
+}
